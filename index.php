054399a29d70c6ca5422ed77349d5bd532971784
daina-r <36935561+daina-r@users.noreply.github.com>
2018-04-11 23:39:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$json = file_get_contents(__DIR__ . '/adressbook.json');
+$data = json_decode($json, true);
+
+?>
+
+<html>
+<head>
+	<title>Адресная книга</title>
+</head>
+<body>
+	
+<table>
+	<thead>
+		<tr>
+			<td>Имя</td>
+			<td>Фамилия</td>
+			<td>Адрес</td>
+			<td>Телефон</td>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($data as $item) : ?>
+		<tr>
+			<td><?=$item['firstName'] ?></td>
+			<td><?=$item['lastName'] ?></td>
+			<td><?=$item['address'] ?></td>
+			<td><?=$item['phoneNumber'] ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
+
+</body>
+</html>
